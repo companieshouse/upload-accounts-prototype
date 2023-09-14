5d02a7993f17f2280d38724fd4c8f62a4ee4d405
James Francis <jfranciswebdesign@users.noreply.github.com>
2023-09-14 09:15:16 +0100

    Correcting routing

--- app/routes.js
+++ app/routes.js
@@ -11,9 +11,9 @@ const router = govukPrototypeKit.requests.setupRouter()
 router.post('/10-check-your-answers', function(request, response) {
 
     var type = request.session.data['type-of-accounts']
-    if (type == "Community Interest Company"){
+    if (type == "Community Interest Company (CIC) accounts"){
         response.redirect("https://products.payments.service.gov.uk/pay/b963568c2e924d1db38baeda7815db76")
-    } else if (type == "Overseas"){
+    } else if (type == "Overseas company accounts"){
         response.redirect("https://products.payments.service.gov.uk/pay/b6270574de6040fc9a76dbf47664c7ca")
     } else {
         response.redirect("/v1/12-confirmation-submission")
